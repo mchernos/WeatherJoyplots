81a6aee197d928bdfa37b15618f9a95dacd737a9
mchernos <mchernos@gmail.com>
2021-06-29 11:44:38 -0600

    Update weather_joyplots.R

--- weather_joyplots.R
+++ weather_joyplots.R
@@ -20,15 +20,6 @@ plot_dat = yyc %>%
   select(Period, date, `Maximum Daily` = max_temp, `Minimum Daily` = min_temp) %>% 
   filter(complete.cases(.))
 
-# 2nd test 
-# plot_dat = read_csv('/Users/mattchernos/Desktop/climate-daily.csv') %>% 
-#   mutate(year = as.numeric(LOCAL_YEAR), 
-#          Period = cut(LOCAL_YEAR, breaks = c(rev(as.numeric(minYr)), Inf), labels = rev(labs))) %>% 
-#   select(Period, date = LOCAL_DATE, `Maximum Daily` = MAX_TEMPERATURE, 
-#          `Minimum Daily` = MIN_TEMPERATURE) %>% 
-#   filter(complete.cases(.))
-
-
 # Make the plot
 plot_dat %>% 
   filter(lubridate::month(date) %in% 6:8) %>%
@@ -37,7 +28,6 @@ plot_dat %>%
   geom_density_ridges_gradient(aes(fill = stat(x)), show.legend = F) +
   # stat_density_ridges(quantile_lines = TRUE, quantiles = 5, alpha = 1, fill = NA) + 
   facet_wrap(~Var, scales = 'free_x', strip.position = 'top') + 
-  # scale_x_sqrt() + 
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_discrete(expand = expand_scale(mult = c(0.01, 0.25))) +
   scale_fill_viridis_c(option = "C") +
